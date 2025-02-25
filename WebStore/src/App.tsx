646fde1a5d33dfcf63d9d0e5c4e941349f3cb3f9
ticinovicsara <ticinovicsara0@gmail.com>
2025-02-25 11:23:42 +0100

    add fetch api

--- WebStore/src/App.tsx
+++ WebStore/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/add-product" element={<AddProductPage />} />
+
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
--- WebStore/src/pages/HomePage.tsx
+++ WebStore/src/pages/HomePage.tsx
@@ -1,12 +1,81 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-const HomePage: React.FC = () => {
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  image: string;
+  category: string;
+};
+
+function HomePage() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products?limit=20");
+        const data = await res.json();
+
+        const localProducts = JSON.parse(
+          localStorage.getItem("products") ?? "[]"
+        );
+
+        setProducts([...data, ...localProducts]);
+
+        const catRes = await fetch(
+          "https://fakestoreapi.com/products/categories"
+        );
+        const catData = await catRes.json();
+        setCategories(catData);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const filteredProducts = products.filter((p) => {
+    return (
+      p.title.toLowerCase().includes(search.toLowerCase()) &&
+      (!category || p.category === category)
+    );
+  });
+
   return (
     <div>
       <h1>Products</h1>
-      <p>Here is a list of all products.</p>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <select onChange={(e) => setCategory(e.target.value)}>
+        <option value="">All categories</option>
+        {categories.map((cat) => (
+          <option key={cat} value={cat}>
+            {cat}
+          </option>
+        ))}
+      </select>
+
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {filteredProducts.map((p) => (
+          <Link key={p.id} to={`/product/${p.id}`}>
+            <img src={p.image} alt={p.title} />
+            <h4>{p.title}</h4>
+            <p>{p.price}</p>
+          </Link>
+        ))}
+      </div>
     </div>
   );
-};
-
+}
 export default HomePage;
